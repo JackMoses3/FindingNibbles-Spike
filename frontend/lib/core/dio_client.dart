193242cc12275ed7846b35d582c4047fdc03b11e
Raphael Schwalb <rsch0027@student.monash.edu>
2025-03-26 15:06:02 +1100

    Adding comment for android emulator

--- frontend/lib/core/dio_client.dart
+++ frontend/lib/core/dio_client.dart
@@ -10,7 +10,7 @@ class DioClient {
   DioClient._internal() {
     dio = Dio(
       BaseOptions(
-        baseUrl: 'http://10.0.2.2:3000',
+        baseUrl: 'http://localhost:3000', // use 'http://10.0.2.2:3000' for android
         connectTimeout: const Duration(seconds: 5),
         receiveTimeout: const Duration(seconds: 5),
         headers: {'Content-Type': 'application/json'},
